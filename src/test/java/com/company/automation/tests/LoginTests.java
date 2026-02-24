ba904702274a68c9fb07ee5e4c29de6b58f8cd2e
Sravanthi1904 <ps422@sciits.com>
2026-02-24 20:20:29 +0530

    changed credentials from admin to customer

--- src/test/java/com/company/automation/tests/LoginTests.java
+++ src/test/java/com/company/automation/tests/LoginTests.java
@@ -11,9 +11,11 @@ public class LoginTests extends BaseTest {
     @Test
     public void validLoginTest() {
         LoginPage login = new LoginPage(driver);
-        login.login("superadmin@pocketark.com", "cx9kGP*3fry4XOX");
+        login.login("saikrishna.pallikonda@radiant.digital", "Radiant@2028");
+        //login.login("superadmin@pocketark.com", "cx9kGP*3fry4XOX");
         //WaitUtils wait = new WaitUtils(driver);
         //wait.waitForUrlContains("dashboard");
+        System.out.println("Customer logged in successfully");
         Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"));
     }
 }
